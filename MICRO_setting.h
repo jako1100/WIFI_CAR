1a6ff0c5a517f40daea4bea56cf28261c13f76e7
jako1100 <37850256+jako1100@users.noreply.github.com>
2019-10-31 14:15:26 +0200

    std_types

---
+++ MICRO_setting.h
@@ -0,0 +1,16 @@
+/*
+ * MICRO_SETTING.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: Mohamed ELameer
+ */
+
+#ifndef MICRO_SETTING_H_
+#define MICRO_SETTING_H_
+
+#include "data_types.h"
+
+
+
+
+#endif /* MICRO_SETTING_H_ */
---
+++ data_types.h
@@ -0,0 +1,32 @@
+/*
+*
+*data_types.h
+*
+*
+*author: Yaakoub
+*
+*/
+
+#ifndef DATA_TYPES_H_INCLUDED
+#define DATA_TYPES_H_INCLUDED
+
+/* Unsigned data types */
+typedef unsigned char      uint8_t  ;
+//typedef unsigned short     uint16_t ;
+typedef unsigned long      uint32_t ;
+typedef unsigned long long uint64_t ;
+
+
+/* Signed data types */
+typedef signed char      int8_t  ;
+//typedef signed short     int16_t ;
+typedef signed long      int32_t ;
+typedef signed long long int64_t ;
+
+
+/* FLoating point data types */
+typedef float      float32_t  ;
+typedef double     float64_t  ;
+
+
+#endif /* DATA_TYPES_H_INCLUDED */
